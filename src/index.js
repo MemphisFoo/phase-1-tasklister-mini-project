f0d127fda8de9b049dbdf12459123223d1438091
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-06-30 16:34:02 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  handleList(e.target.new_task.value)
+  })
+})
+
+function handleList(list){
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = "X"
+  p.textContent=`${list}  `
+document.querySelector('#list').appendChild(p).appendChild(btn)
+btn.addEventListener('click',removeBtn)};
+
+function removeBtn(e){
+  e.target.parentNode.remove()}
+
+ 
+
